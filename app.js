804d66843fb53e05b70b8f66df51ae32e2d858f8
Margo-Bond <ritabond1991@gmail.com>
2024-12-16 21:23:31 +0100

    feat: add alt for images

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
 
 var taskInput = document.getElementById("new-task"); // Add a new task.
 var addButton = document.getElementsByTagName("button")[0]; // First button
-var incompleteTaskHolder = document.getElementById("incompleteTasks"); // ul of #incompleteTasks
+var incompleteTaskHolder = document.getElementById("tasks-todo"); // ul of #incompleteTasks
 var completedTasksHolder = document.getElementById("completed-tasks"); // completed-tasks
 
 // New task list item
@@ -69,7 +69,7 @@ var editTask = function () {
   var editInput = listItem.querySelector('input[type=text]');
   var label = listItem.querySelector("label");
   var editBtn = listItem.querySelector(".edit");
-  var containsClass = listItem.classList.contains("editMode");
+  var containsClass = listItem.classList.contains("edit-mode");
 
   // If class of the parent is .editmode
   if (containsClass) {
@@ -82,7 +82,7 @@ var editTask = function () {
     editBtn.innerText = "Save";
   }
   // Toggle .editmode on the parent.
-  listItem.classList.toggle("editMode");
+  listItem.classList.toggle("edit-mode");
 };
 
 // Delete task.
